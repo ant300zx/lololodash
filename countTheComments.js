8f86a75e14a1900f0f2387e6632639d70fd75df0
Anthony Cull <acull76@gmail.com>
2014-09-25 16:53:58 +0100

    Added the official solution to the file

--- countTheComments.js
+++ countTheComments.js
@@ -22,3 +22,27 @@ var worker = function(data) {
 module.exports = worker;
 
 //OFFICIAL SOLUTION
+ 'use strict';
+
+var _ = require("lodash");
+
+var commentcount = function (comments) {
+
+    var counted = [];
+
+    // Group by article
+    comments = _.groupBy(comments, 'username');
+
+    _.forEach(comments, function (item, name) {
+
+        counted.push({
+            username: name,
+            comment_count: _.size(item)
+        });
+    });
+
+    return _.sortBy(counted, "comment_count").reverse();
+
+};
+
+module.exports = commentcount;
